d8e23b9694fd7ed4c7e8ff6a4e7726b4bb019581
NakagamiYuta <guguru0014@gmail.com>
2024-10-23 16:47:35 +0900

    [add] examples/arithmetic, tests/arithmetic

---
+++ examples/lang_arithmetic/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::{stdin, stdout, Write};
+
+use copager::lex::RegexLexer;
+use copager::parse::LR1;
+use copager::ir::SExp;
+use copager::Processor;
+
+use example_lang_arithmetic::*;
+
+type MyLexer = RegexLexer<ArithmeticToken>;
+type MyParser = LR1<ArithmeticToken, ArithmeticRule>;
+type MyProcessor = Processor<Arithmetic, MyLexer, MyParser>;
+
+fn main() -> anyhow::Result<()> {
+    println!("Example <arithmetic>");
+    print!("Input: ");
+    stdout().flush()?;
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
+
+    let sexp = MyProcessor::new()
+        .build_lexer()?
+        .build_parser()?
+        .process::<SExp<_, _>>(&input)?;
+    println!("Success: {}", sexp);
+
+    Ok(())
+}
