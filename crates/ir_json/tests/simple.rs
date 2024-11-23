191574c79545196ed6efed96ba03c3c557cbf5d4
NakagamiYuta <guguru0014@gmail.com>
2024-11-23 19:10:18 +0900

    [add] ir_json に JsonLite を追加

--- crates/ir_json/tests/simple.rs
+++ crates/ir_json/tests/simple.rs
@@ -4,7 +4,7 @@ use copager_cfl::rule::{RuleTag, Rule, RuleElem};
 use copager_cfl::{CFL, CFLTokens, CFLRules};
 use copager_lex_regex::RegexLexer;
 use copager_parse_lr_lr1::LR1;
-use copager_ir_json::Json;
+use copager_ir_json::{Json, JsonLite};
 
 #[derive(Debug, Default, CFL)]
 struct TestLang (
@@ -50,13 +50,13 @@ enum TestRule {
 }
 
 #[test]
-fn simple_display() {
-    type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
-    type TestProcessor = Processor<TestGenerator<TestLang>>;
-
-    let parse = |input| {TestProcessor::new()
-        .build()?
-        .process::<Json<_>>(input)
+fn normal_display() {
+    let parse = |input| {
+        type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
+        type TestProcessor = Processor<TestGenerator<TestLang>>;
+        TestProcessor::new()
+            .build()?
+            .process::<Json<_>>(input)
     };
 
     let ir = parse("1");
@@ -70,4 +70,28 @@ fn simple_display() {
     let ir = parse("(1 + 1) * 1");
     assert!(ir.is_ok());
     assert_eq!(ir.unwrap().to_string(), r#"{"kind": "list", "tag": "Expr", "elements": [{"kind": "list", "tag": "Term", "elements": [{"kind": "list", "tag": "Term", "elements": [{"kind": "list", "tag": "Num", "elements": [{"kind": "list", "tag": "Expr", "elements": [{"kind": "list", "tag": "Expr", "elements": [{"kind": "list", "tag": "Term", "elements": [{"kind": "list", "tag": "Num", "elements": [{"kind": "token", "tag": "Num", "str": "1"}]}]}]}, {"kind": "token", "tag": "Plus", "str": "+"}, {"kind": "list", "tag": "Term", "elements": [{"kind": "list", "tag": "Num", "elements": [{"kind": "token", "tag": "Num", "str": "1"}]}]}]}]}]}, {"kind": "token", "tag": "Mul", "str": "*"}, {"kind": "list", "tag": "Num", "elements": [{"kind": "token", "tag": "Num", "str": "1"}]}]}]}"#);
+}
+
+#[test]
+fn lite_display() {
+    type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
+    type TestProcessor = Processor<TestGenerator<TestLang>>;
+
+    let parse = |input| {TestProcessor::new()
+        .build()?
+        .process::<JsonLite<_>>(input)
+    };
+
+    let ir = parse("1");
+    assert!(ir.is_ok());
+    assert_eq!(ir.unwrap().to_string(), r#"{"tag": "Expr", "elements": [{"tag": "Term", "elements": [{"tag": "Num", "elements": ["1"]}]}]}"#);
+
+    let ir = parse("1 + 1");
+    assert!(ir.is_ok());
+
+    assert_eq!(ir.unwrap().to_string(), r#"{"tag": "Expr", "elements": [{"tag": "Expr", "elements": [{"tag": "Term", "elements": [{"tag": "Num", "elements": ["1"]}]}]}, "+", {"tag": "Term", "elements": [{"tag": "Num", "elements": ["1"]}]}]}"#);
+
+    let ir = parse("(1 + 1) * 1");
+    assert!(ir.is_ok());
+    assert_eq!(ir.unwrap().to_string(), r#"{"tag": "Expr", "elements": [{"tag": "Term", "elements": [{"tag": "Term", "elements": [{"tag": "Num", "elements": [{"tag": "Expr", "elements": [{"tag": "Expr", "elements": [{"tag": "Term", "elements": [{"tag": "Num", "elements": ["1"]}]}]}, "+", {"tag": "Term", "elements": [{"tag": "Num", "elements": ["1"]}]}]}]}]}, "*", {"tag": "Num", "elements": ["1"]}]}]}"#);
 }
