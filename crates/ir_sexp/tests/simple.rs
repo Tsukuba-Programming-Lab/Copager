731dc0d013cb13c63e0a7d863c983eb10282313b
NakagamiYuta <guguru0014@gmail.com>
2024-11-25 23:51:55 +0900

    [update] ir_sexp のテストを変更

--- crates/ir_sexp/tests/simple.rs
+++ crates/ir_sexp/tests/simple.rs
@@ -4,16 +4,16 @@ use copager_cfl::rule::{RuleTag, Rule, RuleElem};
 use copager_cfl::{CFL, CFLTokens, CFLRules};
 use copager_lex_regex::RegexLexer;
 use copager_parse_lr_lr1::LR1;
-use copager_ir_sexp::SExp;
+use copager_ir_sexp::{SExp, SExpOwned};
 
 #[derive(Default, CFL)]
-struct ExprLang (
-    #[tokens] ExprToken,
-    #[rules]  ExprRule
+struct TestLang (
+    #[tokens] TestToken,
+    #[rules]  TestRule
 );
 
 #[derive(Debug, Default, Copy, Clone, Hash, PartialEq, Eq, CFLTokens)]
-enum ExprToken {
+enum TestToken {
     #[default]
     #[token(r"\+")]
     Plus,
@@ -34,7 +34,7 @@ enum ExprToken {
 }
 
 #[derive(Debug, Default, Copy, Clone, Hash, PartialEq, Eq, CFLRules)]
-enum ExprRule {
+enum TestRule {
     #[default]
     #[rule("<expr> ::= <expr> Plus <term>")]
     #[rule("<expr> ::= <expr> Minus <term>")]
@@ -50,7 +50,31 @@ enum ExprRule {
 }
 
 #[test]
-fn simple_display() {
+fn check_compile() -> anyhow::Result<()> {
+    type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
+    type TestProcessor = Processor<TestGenerator<TestLang>>;
+
+    TestProcessor::new()
+        .build()?
+        .process::<SExp<_>>("1 + 2 * 3")?;
+
+    TestProcessor::new()
+        .build()?
+        .process::<SExpOwned<_>>("1 + 2 * 3")?;
+
+    Ok(())
+}
+
+#[test]
+fn check_display() {
+    let parse = |input| {
+        type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
+        type TestProcessor = Processor<TestGenerator<TestLang>>;
+        TestProcessor::new()
+            .build()?
+            .process::<SExp<_>>(input)
+    };
+
     let ir = parse("1");
     assert!(ir.is_ok());
     assert_eq!(ir.unwrap().to_string(), r#"(Expr (Term (Num "1")))"#);
@@ -62,66 +86,4 @@ fn simple_display() {
     let ir = parse("(1 + 1) * 1");
     assert!(ir.is_ok());
     assert_eq!(ir.unwrap().to_string(), r#"(Expr (Term (Term (Num (Expr (Expr (Term (Num "1"))) "+" (Term (Num "1"))))) "*" (Num "1")))"#);
-}
-
-#[test]
-fn simple_eval() {
-    assert_eq!(eval(&parse("1").unwrap()), 1);
-    assert_eq!(eval(&parse("1 + 2").unwrap()), 3);
-    assert_eq!(eval(&parse("1 + 2 * 3").unwrap()), 7);
-    assert_eq!(eval(&parse("(1 + 2) * 3").unwrap()), 9);
-}
-
-fn parse<'input>(input: &'input str) -> anyhow::Result<SExp<'input, ExprLang>> {
-    type TestGenerator<T> = Generator<T, RegexLexer<T>, LR1<T>>;
-    type TestProcessor = Processor<TestGenerator<ExprLang>>;
-
-    TestProcessor::new()
-        .build()?
-        .process::<SExp<_>>(input)
-}
-
-fn eval(ir: &SExp<'static, ExprLang>) -> i32 {
-    macro_rules! match_atom {
-        ($term:expr, $($kind:literal => $block:expr),* $(,)?) => {
-            match $term {
-                SExp::Atom(token) => {
-                    match token {
-                        $($kind => $block,)*
-                         _ => unreachable!()
-                    }
-                }
-                _ => unreachable!(),
-            }
-        }
-    }
-
-    match ir {
-        SExp::List { rule, elems } => {
-            match rule {
-                ExprRule::Expr if elems.len() == 1 => eval(&elems[0]),
-                ExprRule::Expr => {
-                    let lhs = eval(&elems[0]);
-                    let rhs = eval(&elems[2]);
-                    match_atom!(elems[1],
-                        "+" => lhs + rhs,
-                        "-" => lhs - rhs,
-                    )
-                }
-                ExprRule::Term if elems.len() == 1 => eval(&elems[0]),
-                ExprRule::Term => {
-                    let lhs = eval(&elems[0]);
-                    let rhs = eval(&elems[2]);
-                    match_atom!(elems[1],
-                        "*" => lhs * rhs,
-                        "/" => lhs / rhs,
-                    )
-                }
-                ExprRule::Num if elems.len() == 1 => eval(&elems[0]),
-                ExprRule::Num => eval(&elems[1]),
-
-            }
-        }
-        SExp::Atom(token) => token.parse().unwrap(),
-    }
 }
